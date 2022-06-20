ede678985ba55f759860d2353502932f13821af6
jwabednego <abednegojw@gmail.com>
2022-06-20 14:09:36 -0700

    CORP Error fix

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,7 @@ const items = require("../assets/data");
 
 exports.handler = async (event, context) => {
   return {
+    headers: { "Access-Control-Allow-Orign": "*" },
     statusCode: 200,
     body: JSON.stringify(items),
   };
