165556480c388e1be9e5b94d1620176b8012da51
jwabednego <abednegojw@gmail.com>
2022-06-20 14:20:55 -0700

    CORS Error Fix

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,7 +2,9 @@ const items = require("../assets/data");
 
 exports.handler = async (event, context) => {
   return {
-    headers: { "Access-Control-Allow-Orign": "*" },
+    headers: {
+      "Access-Control-Allow-Orign": "*",
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
